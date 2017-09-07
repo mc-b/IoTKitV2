08bd46333050f46d5f0ae0f4bd8e7a69e1ad772f
mbern <mbern@pad2mcb>
2017-09-07 18:54:01 +0200

    Suche verbessert

--- initide.sh
+++ initide.sh
@@ -3,7 +3,7 @@
 #	Initialisiert die Eclipse CPP Umgebung 
 #
 
-for file in `find . -mindepth 2 -maxdepth 2 -type d -print | grep -v metadata | grep -v .git | grep -v lib | grep -v vagrant | grep -v images`
+for file in `find * -mindepth 1 -maxdepth 1 -type d -print | grep -v metadata | grep -v lib | grep -v vagrant | grep -v images`
 do 
 	cd $file 
 	rm -rf .pio* .hg
